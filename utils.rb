8bea1c6a49a1da8be79000b221ed3ba2754e3264
i-tsvetkov <i.tsvetkov.4@gmail.com>
2015-04-04 19:58:13 +0300

    fixed string regex

--- utils.rb
+++ utils.rb
@@ -12,16 +12,16 @@ end
 # Return the CSS classes with color inside them
 # Does NOT WORK with NESTED rules (e.g. @media)
 def get_classes_with_color(css)
-  # Double quoted strings: /"([^"]|\")*"/
-  # Single quoted strings: /'([^']|\')*'/
+  # Double quoted strings: /"([^\\"]|\\.)*"/
+  # Single quoted strings: /'([^\\']|\\.)*'/
   # Color regex: /#(\h{3}){1,2}\b|\brgba?\([^()]+\)|\bhsla?\([^()]+\)/i
     # hex: /#(\h{3}){1,2}\b/
     # rgb/a: /\brgba?\([^()]+\)/
     # hsl/a: /\bhsla?\([^()]+\)/
   css.scan(/[^{}]+\{
-            (?:[^{}"']|"(?:[^"]|\")*"|'(?:[^']|\')*')*
+            (?:[^{}"']|"(?:[^\\"]|\\.)*"|'(?:[^\\']|\\.)*')*
             (?:\#(?:\h{3}){1,2}\b|\brgba?\([^()]+\)|\bhsla?\([^()]+\))
-            (?:[^{}"']|"(?:[^"]|\")*"|'(?:[^']|\')*')*
+            (?:[^{}"']|"(?:[^\\"]|\\.)*"|'(?:[^\\']|\\.)*')*
            \}/ix)
 end
 
