6b2d265c37a1afba04384c67badaa1810ba46cda
i-tsvetkov <i.tsvetkov.4@gmail.com>
2015-04-08 23:42:04 +0300

    added colorscheme inversion

--- solarized.rb
+++ solarized.rb
@@ -6,6 +6,17 @@ solarized_colors = ["#002b36", "#073642", "#586e75", "#657b83",
                     "#b58900", "#cb4b16", "#dc322f", "#d33682",
                     "#6c71c4", "#268bd2", "#2aa198", "#859900"]
 
+solarized_inverted = { "#fdf6e3" => "#002b36",
+                       "#eee8d5" => "#073642",
+                       "#93a1a1" => "#586e75",
+                       "#839496" => "#657b83",
+
+                       "#002b36" => "#fdf6e3",
+                       "#073642" => "#eee8d5",
+                       "#586e75" => "#93a1a1",
+                       "#657b83" => "#839496"
+                     }
+
 ARGF.argv.each do |file|
   if not File.exist?(file)
     next
@@ -14,6 +25,11 @@ ARGF.argv.each do |file|
   text = File.read(file)
   colors = text.scan(/#(?:\h{3}){1,2}\b|\brgba?\([^()]+\)|\bhsla?\([^()]+\)/i).uniq
   scheme = make_color_scheme(colors, solarized_colors)
+
+  scheme.map!{ |c|
+    solarized_inverted.key?(c[:to].downcase) ?
+      { :from => c[:from], :to => solarized_inverted[c[:to].downcase] } : c
+  }
 
   # replace long strings before short ones
   scheme.sort_by!{ |c| -c[:from].length }
